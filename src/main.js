5951679a87f09b13a5b35e92176248a5498ddc96
JunseungPark <dpfwl8745@gmail.com>
2022-07-29 18:35:26 +0900

    오류 수정

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import BootstrapVue3 from 'bootstrap-vue-3'
 
 // css 적용
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App)
